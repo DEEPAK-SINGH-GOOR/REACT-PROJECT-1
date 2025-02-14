683bb4e7898f1f680b8e47701fa3e4a315d79672
deepak <deepaksinghgoor745@gmail.com>
2025-02-14 09:03:25 +0530

    Project

--- client/src/routes/Private.jsx
+++ client/src/routes/Private.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { getUserDetails } from "../userDetails";
+import { Navigate } from "react-router-dom";
 
-const Private = () => {
-  return (
-    <div>Private</div>
-  )
-}
+const Private = ({children}) => {
+  let user = getUserDetails();
 
-export default Private
+  if (user) {
+    return children
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
+
+export default Private;
